22d0e1d56487fd35baef8b3690b10ca5af195a5b
Bang241002 <bang241002@gmail.com>
2022-03-25 16:43:46 +0700

    Create vidu5.java

---
+++ task1/vidu5.java
@@ -0,0 +1,18 @@
+public class vidu5 {
+    public static void main(string[] args){
+
+        double radius, area, circumference;
+
+        final  double PI = 3.14159265;
+
+        radius = 1.2;
+
+        area = radius * radius * PI;
+        system.out.print("the radius is ");
+        system.out.println("radius");
+        system.out.print("the area is ");
+        system.out.println("area ");
+        system.out.print("the circumferece is ");
+        system.out.println("circumferece ");
+    }
+}
