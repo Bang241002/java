85e478efabe3e306837487bce1ad3cf66487d3e0
Bang241002 <bang241002@gmail.com>
2022-03-25 16:43:42 +0700

    Create vidu3.java

---
+++ task1/vidu3.java
@@ -0,0 +1,15 @@
+public class vidu3 {
+    public static void main (string[] args){
+        System.out.println("hello world!");
+        System.out.println("hello world again!");
+        System.out.println();
+        System.out.print("hello world");
+        System.out.print("hello world again!");
+        System.out.println();
+        System.out.println("hello,");
+        System.out.println(" ");
+        System.out.println("world !");
+        System.out.println("hello , world!");
+
+    }
+}
