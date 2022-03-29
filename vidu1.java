d2241b4909385036df764feb79e5f89e7caadf4e
Bang241002 <91303671+Bang241002@users.noreply.github.com>
2022-03-29 17:52:41 +0700

    task2

---
+++ vidu1.java
@@ -0,0 +1,16 @@
+public class vidu1 {
+    double radius ;
+    string color;
+
+    double getRadius(){ ...... }
+    double getRadius(){ ...... }
+}
+
+public class soccerPlayer{
+    int number;
+    String name;
+    int x,y;
+
+    void run(){......}
+    void kickball(){......}
+}
---
+++ vidu4.java
@@ -0,0 +1,29 @@
+
+
+
+public class Circle {
+    private double radius ;
+    private String color ;
+
+    public Cricle (){
+        radius = 1.0;
+        color = "red";
+    }
+    public Circle(double r){
+        radius = r;
+        color ="red";
+    }
+    public Circle(double r, String c){
+        radius = r;
+        color = c;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+    public string getcolor(){
+        return color;
+    }
+    public radius  * radius * Math.PI;
+}
+
