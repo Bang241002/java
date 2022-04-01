e2e52237f357ab674f58c1d553a324eb38c3eafb
Bang241002 <bang241002@gmail.com>
2022-04-01 17:36:11 +0700

    Create circle2.java

---
+++ jdk/src/circle2.java
@@ -0,0 +1,19 @@
+public class circle2 {
+    private double radius;
+
+    public circle2(){
+        radius = 1.0;
+    }
+
+    public circle2(double r){
+        radius = r;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double tostring(){
+        return tostring;
+    }
+}
