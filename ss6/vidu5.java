21a60823202fb7b995e5d717e376b2d85bc96bfb
Bang241002 <bang241002@gmail.com>
2022-04-09 23:48:05 +0700

    Create vidu5.java

---
+++ ss6/vidu5.java
@@ -0,0 +1,20 @@
+package ss6;
+
+
+public class vidu5 {
+
+    final float PI = 3.14F;
+
+    public void display(float pi){
+        PI = pi;
+        System.out.println("the value cf PI is :" +PI);
+    }
+
+    public static void main(String[]args){
+
+        final Final Demo objFinal Demo = new FinalDemo();
+
+        objFinalDemo.display(22.7F);
+    }
+
+}
