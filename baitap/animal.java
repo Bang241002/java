0a5d9286a668fdae09fd0ed5579cabcc522da12b
Bang241002 <bang241002@gmail.com>
2022-04-18 21:06:39 +0700

    Create animal.java

---
+++ baitap/animal.java
@@ -0,0 +1,41 @@
+package thuake;
+
+//public class animal {
+//    private String name;
+//
+//    public animal(String name){
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return "animal[name=" + name + "]";
+//    }
+//}
+
+//public class animal {
+//    private String name;
+//
+//    public animal(String name){
+//        this.name = name;
+//    }
+//
+//    @Override
+//    public String toString(){
+//        return "animal[name=" + name + "]";
+//    }
+//}
+
+public class animal {
+    private String name;
+
+    public animal(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "animal[name=" + name + "]";
+    }
+}
+
