204c634948202395a45f5b860524cd67e41946a1
Bang241002 <bang241002@gmail.com>
2022-04-18 21:06:49 +0700

    Create testanimal.java

---
+++ baitap/testanimal.java
@@ -0,0 +1,35 @@
+package thuake;
+
+//public class testanimal {
+//    public static void main(String[] args) {
+//        animal a1 = new animal("bo");
+//        cat meo = new cat("cho");
+//        dog cho = new dog("meo");
+//        System.out.println(a1);
+//        meo.greets();
+//        cho.greets();
+//    }
+//
+//}
+
+//public class testanimal {
+//    public static void main(String[] args) {
+//        animal a1 = new animal("bo");
+//        cat meo = new cat("cho");
+//        dog cho = new dog("meo");
+//        System.out.println(a1);
+//        meo.greets();
+//        cho.greets();
+//    }
+//}
+
+public class testanimal {
+    public static void main(String[] args) {
+        animal a1 = new animal("bo");
+        cat meo = new cat("cho");
+        dog cho = new dog("meo");
+        System.out.println(a1);
+        meo.greets();
+        cho.greets();
+    }
+}
