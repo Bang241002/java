b1caa8c9390165569a73be2fc71c7bf640faed9b
Bang241002 <bang241002@gmail.com>
2022-04-18 21:06:44 +0700

    Create dog.java

---
+++ baitap/dog.java
@@ -0,0 +1,58 @@
+package thuake;
+
+//public class dog extends mamal{
+//
+//    public dog(String name) {
+//        super(name);
+//    }
+//
+//    public void greets(){
+//        System.out.println("woof");
+//    }
+//
+//    public void greets(dog another){
+//        System.out.println("woooof");
+//    }
+//
+//    public String toString() {
+//        return "dog[" +super.toString() + "]";
+//    }
+//}
+
+//public class dog extends mamal{
+//
+//    public dog(String name) {
+//        super(name);
+//    }
+//
+//    public void greets(){
+//        System.out.println("woof");
+//    }
+//
+//    public void greets(dog another){
+//        System.out.println("woooof");
+//    }
+//
+//    public String toString() {
+//        return "dog[" +super.toString() + "]";
+//    }
+//}
+
+public class dog extends mamal{
+
+    public dog(String name) {
+        super(name);
+    }
+
+    public void greets(){
+        System.out.println("woof");
+    }
+
+    public void greets(dog another){
+        System.out.println("woooof");
+    }
+
+    public String toString() {
+        return "dog[" +super.toString() + "]";
+    }
+}
