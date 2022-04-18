6e29bbab244a23948d4f7f989439c3762234f9c3
Bang241002 <bang241002@gmail.com>
2022-04-18 21:06:46 +0700

    Create mamal.java

---
+++ baitap/mamal.java
@@ -0,0 +1,34 @@
+package thuake;
+
+//public class mamal extends  animal{
+//    public mamal(String name){
+//        super(name);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "mamal[" + super.toString() + "]";
+//    }
+//}
+
+//public class mamal extends  animal{
+//    public mamal(String name){
+//        super(name);
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "mamal[" + super.toString() + "]";
+//    }
+//}
+
+public class mamal extends  animal{
+    public mamal(String name){
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "mamal[" + super.toString() + "]";
+    }
+}
