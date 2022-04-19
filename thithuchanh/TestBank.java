9ed4f681b5f2bd9dd7cebe8618bd1b541ceecfcb
Bang241002 <bang241002@gmail.com>
2022-04-19 09:49:51 +0700

    Create TestBank.java

---
+++ thithuchanh/TestBank.java
@@ -0,0 +1,9 @@
+package java.thithuchanh;
+
+public class TestBank {
+    public static void main(String[]args){
+        Bank b1 = new Bank(10000, 10);
+
+        System.out.println(b1.calculatelnterest());
+    }
+}
